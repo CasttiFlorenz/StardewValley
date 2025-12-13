88ce031b4783737d99be1ba4ccffa250cbd33e8f
ZRY <zhaoruiyan15@gmail.com>
2025-12-13 20:44:50 +0800

    Enhance TimeManager with day management features
    Added new methods for day management and input control.

--- Classes/TimeManager.h
+++ Classes/TimeManager.h
@@ -4,37 +4,44 @@
 #include "cocos2d.h"
 #include "GameTime.h"
 #include "WeatherManager.h"
+#include <functional>
 
 class TimeManager : public cocos2d::Node {
-public:
+public: 
     static TimeManager* getInstance();
-    virtual bool init(); // 简化初始化，参数写死在内部
-    void update(float dt) override; // 必须有update才能走时间
+    virtual bool init();
+    void update(float dt) override;
 
-    // 获取当前时间数据
+    void triggerPassOut();
+    void startNextDay();
+
+    bool isInputAllowed() const { return _isInputAllowed; }
+    void setInputAllowed(bool val) { _isInputAllowed = val; }
+
     GameTime getCurrentTime() const { return currentTime; }
+
+    std::function<void()> onDayStartCallback;
 
 private:
     TimeManager();
     ~TimeManager();
     static TimeManager* instance;
-    cocos2d::Sprite* weatherIcon;
-    WeatherType currentWeather;
-    // 核心数据
+
     GameTime currentTime;
-    float accumulatedTime; // 累积时间
+    float accumulatedTime;
+    bool _isInputAllowed;
+    bool _isTransitioning;
 
-    // UI 控件
-    cocos2d::Node* uiContainer;   // 整个时钟的容器
-    cocos2d::Sprite* clockBg;       // 背景图
-    cocos2d::Node* clockHand;     // 指针
-    cocos2d::Label* timeLabel;     // 时间文字
-    cocos2d::Label* dateLabel;     // 日期文字
-    cocos2d::Label* moneyLabel;    // 金币文字
+    cocos2d::Node* uiContainer;
+    cocos2d::Sprite* clockBg;
+    cocos2d::Sprite* clockHand;
+    cocos2d::Sprite* weatherIcon; 
+    cocos2d::Label* timeLabel;
+    cocos2d::Label* dateLabel;
+    cocos2d::Label* moneyLabel;
 
-    // 内部方法
     void createUI();
-    void refreshUI(); // 刷新显示
+    void refreshUI();
 };
 
 #endif // __TIME_MANAGER_H__
