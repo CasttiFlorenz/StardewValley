389bd0e1aa5f076797fdb700f23cec28c0c2c48d
Coffee <3385297131@qq.com>
2025-12-15 23:04:13 +0800

    Update Constant.h

--- Classes/Constant.h
+++ Classes/Constant.h
@@ -6,16 +6,16 @@
 
 USING_NS_CC;
 
-// ��ͼ���ű���
+// 地图缩放比例
 constexpr float TILED_MAP_SCALE = 5.0f;
-// ���Ĭ���ٶ�
+// 玩家默认速度
 constexpr float DEFAULT_SPEED = 250.0f;
 
-// ũ���ͼ���ߣ���Ƭ����
+// 农舍地图宽高（瓦片数）
 constexpr int FARM_HOUSE_WIDTH = 20;
 constexpr int FARM_HOUSE_HEIGHT = 12;
 
-// ����ö��
+// 方向枚举
 enum class Direction
 {
     DOWN = 0,
@@ -32,6 +32,36 @@ enum class MouseEvent
     GET_WOOD,
     GET_GRASS
 };
+
+enum class Objects {
+    HOE = 0,           // 锄头
+    AXE,              // 斧头
+    WATERING_CAN,     // 浇水壶
+    PICKAXE,          // 镐
+    SCYTHE,           // 镰刀
+    FISHINGROD,       // 钓鱼竿
+    STONE,            // 石头
+    WOOD,             // 木头
+    COPPER_ORE,       // 铜矿石
+    FERTILIZER,       // 肥料
+    PARSNIP_SEED,     // 防风草种子 
+    CAULIFLOWER_SEED, // 花椰菜种子
+    POTATO_SEED,      // 土豆种子
+    PARSNIP,          // 防风草
+    CAULIFLOWER,      // 花椰菜
+    POTATO,           // 土豆
+    FIBER,            // 纤维
+    DAFFODILS,        // 黄水仙
+    LEEK,             // 韭葱
+    HAY,              // 干草
+    EGG,              // 鸡蛋
+    FRIED_EGG,        // 煎鸡蛋
+    CARP,             // 鲤鱼
+    MILK,             // 牛奶
+    SALAD,            // 田园沙拉
+    COUNT             // 总数，用于遍历
+};
+
 
 enum class KeyBoardEvent
 {
