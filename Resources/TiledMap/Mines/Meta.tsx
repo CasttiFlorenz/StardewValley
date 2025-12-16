9b0ffcac4c5698c6a38287ffe94280b361a7988a
CasttiFlorenz <3252380599@qq.com>
2025-12-16 23:28:48 +0800

    更改了某些object的判定范围

--- Resources/TiledMap/Mines/Meta.tsx
+++ Resources/TiledMap/Mines/Meta.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Meta" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="Pictures/Meta.png" width="64" height="64"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Meta" tilewidth="16" tileheight="16" tilecount="288" columns="4">
+ <image source="../FarmHouse/Pictures/Meta.png" width="64" height="64"/>
  <tile id="0">
   <properties>
    <property name="Collidable" type="bool" value="true"/>
