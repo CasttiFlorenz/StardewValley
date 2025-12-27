4b29a07d5b8ef4b23d643d1413face0287420c07
Coffee <3385297131@qq.com>
2025-12-27 16:31:47 +0800

    Add files via upload

---
+++ Classes/NPC/SocialLevel.h
@@ -0,0 +1,25 @@
+#ifndef __SOCIAL_LEVEL_H__
+#define __SOCIAL_LEVEL_H__
+
+#include "cocos2d.h"
+#include "NPCManager.h" 
+#include "NPCBase.h"
+
+class SocialLevel {
+public:
+    // 创建社交界面
+    static cocos2d::Node* createSocialInterface(cocos2d::Node* parent, cocos2d::Sprite* background);
+
+private:
+    // 创建单个社交人物项
+    static cocos2d::Node* createCharacterItem(NPCBase* npc, const cocos2d::Vec2& position);
+
+    // 创建好感度爱心显示
+    static cocos2d::Node* createFriendshipHearts(int level);
+
+    // 获取NPC头像数据
+    static void getNPCStyle(const std::string& name, std::string& outIconPath, float& outScale);
+
+};
+
+#endif
