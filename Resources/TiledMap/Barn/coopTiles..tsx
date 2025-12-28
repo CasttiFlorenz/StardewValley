d01e0ea3dbaee5040106d10630cb07998f76dff2
CasttiFlorenz <3252380599@qq.com>
2025-12-28 16:52:01 +0800

    Delete Resources/TiledMap directory

--- Resources/TiledMap/Barn/coopTiles..tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="coopTiles." tilewidth="16" tileheight="16" tilecount="84" columns="4">
- <image source="Pictures/coopTiles..png" width="64" height="336"/>
-</tileset>
--- Resources/TiledMap/FarmHouse/furniture.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="furniture" tilewidth="16" tileheight="16" tilecount="2048" columns="32">
- <image source="Pictures/furniture.png" width="512" height="1024"/>
- <tile id="1005">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="8.63636" y="8.18182"/>
-  </objectgroup>
- </tile>
-</tileset>
