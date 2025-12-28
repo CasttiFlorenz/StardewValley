c8a07fb870bd5bf3134f6f7ed5b28814f61f296d
Floloc <1516233391@qq.com>
2025-12-28 20:04:59 +0800

    Update GrassItem.h

--- Classes/MapItem/GrassItem.h
+++ Classes/MapItem/GrassItem.h
@@ -1,14 +1,23 @@
+/****************************************************************
+ * Project Name:  StardewValley
+ * File Name:     GrassItem.h
+ * File Function: GrassItem类的实现
+ * Author:        郭芷烟
+ * Update Date:   2025/12/16
+ * License:       MIT License
+ ****************************************************************/
 #pragma once
 #ifndef __GRASS_ITEM_H__
 #define __GRASS_ITEM_H__
 
 #include "EnvironmentItem.h"
 
-// �ݴ���Ʒ
+// ²Ý´ÔÎïÆ·
 class GrassItem : public EnvironmentItem {
 public:
     static GrassItem* create(const cocos2d::Vec2& tileCoord);
     bool init(const cocos2d::Vec2& tileCoord);
 };
+
 
 #endif // __GRASS_ITEM_H__
