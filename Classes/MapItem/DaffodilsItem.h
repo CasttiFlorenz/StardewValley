cdc2e4e7f2deb9dac518fd1216793351a72eb58c
Floloc <1516233391@qq.com>
2025-12-28 20:02:19 +0800

    Update DaffodilsItem.h

--- Classes/MapItem/DaffodilsItem.h
+++ Classes/MapItem/DaffodilsItem.h
@@ -1,14 +1,23 @@
+/****************************************************************
+ * Project Name:  StardewValley
+ * File Name:     DaffodilsItem.h
+ * File Function: DaffodilsItem类的实现
+ * Author:        郭芷烟
+ * Update Date:   2025/12/16
+ * License:       MIT License
+ ****************************************************************/
 #pragma once
 #ifndef __DAFFODILS_ITEM_H__
 #define __DAFFODILS_ITEM_H__
 
 #include "EnvironmentItem.h"
 
-// ��ˮ����Ʒ
+// »ÆË®ÏÉÎïÆ·
 class DaffodilsItem : public EnvironmentItem {
 public:
     static DaffodilsItem* create(const cocos2d::Vec2& tileCoord);
     bool init(const cocos2d::Vec2& tileCoord);
 };
+
 
 #endif // __DAFFODILS_ITEM_H__
