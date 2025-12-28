4f988cb5fd5a87e1ff99c094c7ebd0cd0aa3591f
Floloc <1516233391@qq.com>
2025-12-28 20:06:08 +0800

    Update LeekItem.cpp

--- Classes/MapItem/LeekItem.cpp
+++ Classes/MapItem/LeekItem.cpp
@@ -1,6 +1,14 @@
+/****************************************************************
+ * Project Name:  StardewValley
+ * File Name:     LeekItem.cpp
+ * File Function: LeekItem类的实现
+ * Author:        郭芷烟
+ * Update Date:   2025/12/16
+ * License:       MIT License
+ ****************************************************************/
 #include "LeekItem.h"
 
-// ����ʵ��
+// ´´½¨ÊµÀý
 LeekItem* LeekItem::create(const cocos2d::Vec2& tileCoord) {
     auto p = new (std::nothrow) LeekItem();
     if (p && p->init(tileCoord)) {
@@ -11,7 +19,7 @@ LeekItem* LeekItem::create(const cocos2d::Vec2& tileCoord) {
     return nullptr;
 }
 
-// ��ʼ��
+// ³õÊ¼»¯
 bool LeekItem::init(const cocos2d::Vec2& tileCoord) {
     if (!EnvironmentItem::init(EnvironmentItemType::LEEK, tileCoord)) {
         return false;
@@ -19,4 +27,5 @@ bool LeekItem::init(const cocos2d::Vec2& tileCoord) {
 
     this->setTexture(LEEK_ITEM_TEXTURE_PATH);
     return true;
+
 }
