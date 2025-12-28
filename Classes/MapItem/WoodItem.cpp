655767575e9d0f191644e46ca9da1a7da00207f5
Floloc <1516233391@qq.com>
2025-12-28 20:08:17 +0800

    Update WoodItem.cpp

--- Classes/MapItem/WoodItem.cpp
+++ Classes/MapItem/WoodItem.cpp
@@ -1,6 +1,14 @@
+/****************************************************************
+ * Project Name:  StardewValley
+ * File Name:     WoodItem.cpp
+ * File Function: WoodItem类的实现
+ * Author:        郭芷烟
+ * Update Date:   2025/12/16
+ * License:       MIT License
+ ****************************************************************/
 #include "WoodItem.h"
 
-// ����ʵ��
+// ´´½¨ÊµÀý
 WoodItem* WoodItem::create(const cocos2d::Vec2& tileCoord) {
     auto p = new (std::nothrow) WoodItem();
     if (p && p->init(tileCoord)) {
@@ -11,7 +19,7 @@ WoodItem* WoodItem::create(const cocos2d::Vec2& tileCoord) {
     return nullptr;
 }
 
-// ��ʼ��
+// ³õÊ¼»¯
 bool WoodItem::init(const cocos2d::Vec2& tileCoord) {
     if (!EnvironmentItem::init(EnvironmentItemType::WOOD, tileCoord)) {
         return false;
@@ -19,4 +27,5 @@ bool WoodItem::init(const cocos2d::Vec2& tileCoord) {
 
     this->setTexture(WOOD_ITEM_TEXTURE_PATH);
     return true;
+
 }
