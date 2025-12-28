16351a7bcc3b816ec220dd8f5f41d9535da81b7e
CasttiFlorenz <3252380599@qq.com>
2025-12-28 23:16:03 +0800

    整理资源文件

---
+++ Resources/TiledMap/FarmHouse/walls_and_floors..tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="walls_and_floors." tilewidth="16" tileheight="16" tilecount="512" columns="16">
+ <image source="Pictures/walls_and_floors..png" width="256" height="512"/>
+ <tile id="335">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.45455" y="6.90909"/>
+  </objectgroup>
+ </tile>
+</tileset>
